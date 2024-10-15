e4407d5721f17618a44e7ff4f87d711e3712827d
Sarfaraz Unar <130893009+sarfarazunarr@users.noreply.github.com>
2024-10-15 21:39:45 +0500

    Update page.tsx

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -7,6 +7,7 @@ const ProjectsPage = () => {
             <h3 className='text-4xl pt-5 text-white' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">Some Cool Projects</h3>
             <p className='text-gray-300 pt-3 pb-10' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="700">Here are some projects you can find more on my github profile!</p>
             <Projects title='no' btnLink='/contact' btnText='Get In Touch' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="900" />
+            <p className='text-xs text-center text-gray-400'>You can view more projects on <Link className='text-green-400 underline' href={"https://github.com/sarfarazunarr"}>Github</Link> and on <Link className='text-green-400 underline' href={"https://www.linkedin.com/in/sarfarazunar/"}>Linkedin</Link></p>
         </main>
     )
 }
