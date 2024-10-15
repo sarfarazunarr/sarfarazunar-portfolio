c9b193de54317211b36c235baa55b103953f64a8
Sarfaraz Unar <sarfarazunarr@gmail.com>
2024-10-15 17:48:19 +0500

    some data added

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -13,6 +13,20 @@ const projects = [
         github: "https://github.com/sarfarazunarr/invoicy",
         video: "https://www.linkedin.com/posts/sarfarazunar_laravel-crud-authentication-activity-7228364008334999552-i8YW?utm_source=share&utm_medium=member_desktop",
     },
+    {
+        img: "/projects/notepadai.jpg",
+        title: "Notepad AI",
+        description: "I have redesigned Notepad using Tkinter & Python where i have integrated ChatGPT API. Where users can generate at content instantly inside notepad.",
+        github: "https://github.com/sarfarazunarr/notepad-ai",
+        video: "https://www.linkedin.com/posts/sarfarazunar_gpt-ai-g4f-activity-7228113152545067009-Oz-9?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+        img: "/projects/javascriptai.jpg",
+        title: "JavaScript AI",
+        description: "It is the best app created using React and FastAPI that generates, debug, explain javascript code. I have integrated AI in this for these tasks.",
+        github: "https://github.com/sarfarazunarr/javascript-ai",
+        video: "https://www.linkedin.com/posts/https://www.linkedin.com/posts/sarfarazunar_ai-gpt-javascript-activity-7235241203603017728-hecE?utm_source=share&utm_medium=member_desktopsarfarazunar_gpt-ai-g4f-activity-7228113152545067009-Oz-9?utm_source=share&utm_medium=member_desktop",
+    },
 
 ]
 
