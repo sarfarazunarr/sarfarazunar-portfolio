427d7518b32cb63ecefe0047b52be2be3c7cee01
Sarfaraz Unar <130893009+sarfarazunarr@users.noreply.github.com>
2024-10-15 21:37:56 +0500

    Update reviews.ts

--- src/app/data/reviews.ts
+++ src/app/data/reviews.ts
@@ -1,19 +1,19 @@
 const reviews = [
     {
         stars: 5,
-        content: "Sarfaraz is an exceptional web developer who provided us with a top-notch website. Her attention to detail, creativity, and dedication to understanding our brand made the process seamless and enjoyable. We're thrilled with the results! The website exceeded our expectations and has received numerous compliments from our customers.",
+        content: "Sarfaraz is an exceptional web developer who provided us with a top-notch website. His attention to detail, creativity, and dedication to understanding our brand made the process seamless and enjoyable. We're thrilled with the results! The website exceeded our expectations and has received numerous compliments from our customers.",
         name: "Ammad hussain",
         img: "/users/user1.jpg"
     },
     {
         stars: 5,
-        content: "Working with Sarfaraz was an absolute pleasure. She is a talented web developer who takes the time to understand your vision and brings it to life. Her professionalism, communication skills, and passion for delivering high-quality work made our experience truly exceptional. We couldn't be more pleased with our website!",
+        content: "Working with Sarfaraz was an absolute pleasure. He is a talented web developer who takes the time to understand your vision and brings it to life. His professionalism, communication skills, and passion for delivering high-quality work made our experience truly exceptional. We couldn't be more pleased with our website!",
         name: "Ghazanfar abbas",
         img: "/users/user2.jpg"
     },
     {
         stars: 4,
-        content: "Sarfaraz did an excellent job designing and developing our website. She is very knowledgeable and was able to provide valuable insights and suggestions throughout the process. The website is user-friendly, visually appealing, and easy to maintain. We would highly recommend her services to anyone looking for a professional web developer.",
+        content: "Sarfaraz did an excellent job designing and developing our website. He is very knowledgeable and was able to provide valuable insights and suggestions throughout the process. The website is user-friendly, visually appealing, and easy to maintain. We would highly recommend his services to anyone looking for a professional web developer.",
         name: "Javed Iqbal",
         img: "/users/user3.jpg"
     },
